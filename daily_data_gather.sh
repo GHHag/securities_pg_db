ad00c17ea7846a1969f5e1fc1ee30d4090439161
GHHag <gustav.hagenblad@gmail.com>
2022-07-06 11:32:22 +0200

    logging

--- daily_data_gather.sh
+++ daily_data_gather.sh
@@ -3,7 +3,8 @@
 
 alias server_path='cd api'
 alias dal_path='cd ../securities_db_py_dal/securities_db_py_dal'
-alias generate_signals_path='cd ../../../systems/system_development/live_systems'
+#alias generate_signals_path='cd ../../../systems/system_development/live_systems'
+alias system_handler_path='cd ../../../systems/system_development/live_systems'
 
 server_path
 node server.js &
@@ -11,5 +12,10 @@ node server.js &
 dal_path
 python dal.py
 
-generate_signals_path
-python generate_signals.py
+#generate_signals_path
+#python generate_signals.py
+system_handler_path
+python system_handler.py
+
+#cd ../../../securities_pg_db/api
+#exit node program
--- securities_db_py_dal/securities_db_py_dal/dal.py
+++ securities_db_py_dal/securities_db_py_dal/dal.py
@@ -2,6 +2,7 @@ import json
 import datetime as dt
 import pytz
 import requests
+import logging
 
 from yahooquery import Ticker
 
@@ -32,7 +33,9 @@ def get_yahooquery_data(
         data.reset_index(inplace=True)
         return data
     except (KeyError, AttributeError, TypeError):
-        print('Error trying to fetch data', instrument) # log error
+        logging.error(
+            f'\n\tERROR while trying to fetch data with yhq. Instrument: {instrument}\n'
+        )
 
 
 def exchange_post_req(exchange_data):
@@ -44,7 +47,8 @@ def exchange_post_req(exchange_data):
         }
     )
 
-    return exchange_post_res.content # log res.content
+    logging.info(f'\n\tEXCHANGE POST REQUEST:\n\t{exchange_post_res.content}\n')
+    return exchange_post_res.content
 
 
 def exchange_get_req(exchange_name):
@@ -59,7 +63,8 @@ def instrument_post_req(exchange_id, symbol):
         data={"symbol": symbol}
     )
 
-    return instrument_post_res.content # log res.content
+    logging.info(f'\n\tINSTRUMENT POST REQUEST:\n\t{instrument_post_res.content}\n')
+    return instrument_post_res.content
 
 
 def instrument_get_req(symbol):
@@ -74,7 +79,8 @@ def price_data_post_req(instrument_id, df_json):
         data={"data": json.dumps(df_json['data'])}
     )
 
-    return price_data_post_res.content # log res.content    
+    logging.info(f'\n\tPRICE DATA POST REQUEST:\n\t{price_data_post_res.content}\n')
+    return price_data_post_res.content  
 
 
 def price_data_get_req(symbol, start_date_time, end_date_time):
@@ -90,16 +96,15 @@ def price_data_get_req(symbol, start_date_time, end_date_time):
 def post_daily_data(
     symbols_list, exchange_name, start_date, end_date, omxs_stock=False
 ):
-    df_none_symbols = []
     for symbol in symbols_list:
         df = get_yahooquery_data(
             symbol, start_date=start_date, end_date=end_date, omxs_stock=omxs_stock
         )
 
+        exception_none_df_symbols = ''
         if df is None or len(df) == 0:
-            df_none_symbols.append(symbol)
+            exception_none_df_symbols += f'{symbol}, '
         else:
-            print(symbol, len(df))
             df_json = json.loads(df.to_json(orient='table'))
 
             try:
@@ -111,16 +116,25 @@ def post_daily_data(
                 instrument_get_res = instrument_get_req(symbol)
                 instrument_id = instrument_get_res['data'][0]['id']
 
-                print(price_data_post_req(instrument_id, df_json))
+                price_data_post_req(instrument_id, df_json)
 
             except Exception:
-                print('EXCEPTION', symbol)
-                df_none_symbols.append(symbol)
+                logging.error(
+                    f'\n\t'
+                    f'EXCEPTION raised while attempting to POST data for {symbol}'
+                )
+                exception_none_df_symbols += f'{symbol}, '
 
-    print(df_none_symbols) # log error generating symbols
+    logging.warning(
+        f"\n\t"
+        f"Symbols where conditional: 'df is None or len(df) == 0:' resulted in True\n\t"
+        f"Symbols: {exception_none_df_symbols}\n"
+    )
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename=f'{env.LOG_FILE_PATH}\stonkinator_log.log', level=logging.INFO)
+
     INSTRUMENTS_DB = InstrumentsMongoDb(env.LOCALHOST_MONGO_DB_URL, 'instruments_db')
 
     #omxs_stock_symbols_list = json.loads(INSTRUMENTS_DB.get_omxs30_instruments())
@@ -149,16 +163,18 @@ if __name__ == '__main__':
         }
     }
 
-    start_date = dt.datetime(2022, 6, 1, tzinfo=pytz.timezone('Europe/Berlin'))
+    start_date = dt.datetime(2022, 6, 25, tzinfo=pytz.timezone('Europe/Berlin'))
     end_date = dt.datetime.now(tz=pytz.timezone('Europe/Berlin'))
     dt_now = dt.datetime.now(tz=pytz.timezone('Europe/Berlin'))
 
-    print(
-        f'\nInsert data\n'
-        f'Start date: {start_date.strftime("%d-%m-%Y")}\n'
+    logging.info(
+        f'\n\t'
+        f'Insert data\n\t'
+        f'Current datetime: {dt_now}\n\t'
+        f'Start date: {start_date.strftime("%d-%m-%Y")}\n\t'
         f'End date: {end_date.strftime("%d-%m-%Y")}\n'
-        f'proceed? y/n'
     )
+
     yes_no_input = 'y' # input('Enter: ')
     if yes_no_input.lower() == 'y':
         for exchange, exchange_data in exchanges_dict.items():
@@ -171,12 +187,20 @@ if __name__ == '__main__':
             if exchange == 'omxs':
                 omxs_stock = True
                 if end_date_today_check and dt_now.hour < 18:
-                    print(end_date_today_check, 'subtracting one day')
                     end_date = end_date - dt.timedelta(days=1)
+                    logging.info(
+                        f'\n\t'
+                        f'Date check (omxs): {end_date_today_check}, subtracting one day\n\t'
+                        f'New end date: {end_date}\n'
+                    )
             else:
                 if end_date_today_check:
-                    print(end_date_today_check, 'subtracting one day')
                     end_date = end_date - dt.timedelta(days=1)
+                    logging.info(
+                        f'\n\t'
+                        f'Date check: {end_date_today_check}, subtracting one day\n\t'
+                        f'New end date: {end_date}\n'
+                    )
 
             post_daily_data(
                 exchange_data['symbols'], exchange, 
